829af4e0e086e0734e423b6f48c92347a72e927d
Nestor Mamani <nmamanipantoja@gmail.com>
2021-09-05 16:36:56 -0500

    Colors

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="bg-yellow-500 text-white">Hello World</h1>
+      <input
+        type="text"
+        className="border border-blue-500 placeholder-green-300"
+        placeholder="Lorem Ipsum"
+      />
+    </div>
+  );
+};
+
+export default Home;
